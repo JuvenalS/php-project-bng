121b1721197bdbed56d1f13789c1ad425e99d734
Juvenal Silveira da Silva Neto <silveiraneto@hotmail.com>
2024-08-16 12:09:57 -0300

    implementação de alteração de senha

--- php/src/app/views/profile_change_password_frm.php
+++ php/src/app/views/profile_change_password_frm.php
@@ -4,7 +4,7 @@
             <div class="card p-4 mb-5">
 
 
-                <form action="#" method="post">
+                <form action="?ct=main&mt=change_password_submit" method="post" novalidate>
                     <div class="row justify-content-center">
                         <div class="col-8">
 
@@ -14,32 +14,53 @@
 
                             <div class="mb-3">
                                 <label for="text_current_password" class="form-label">Password atual</label>
-                                <input type="password" name="text_current_password" id="text_current_password" class="form-control" required>
+                                <input type="password" name="text_current_password" id="text_current_password"
+                                    class="form-control" required>
                             </div>
 
                             <div class="mb-3">
                                 <label for="text_new_password" class="form-label">Nova password</label>
-                                <input type="password" name="text_new_password" id="text_new_password" class="form-control" required>
+                                <input type="password" name="text_new_password" id="text_new_password"
+                                    class="form-control" required>
                             </div>
 
                             <div class="mb-4">
                                 <label for="text_repeat_new_password" class="form-label">Repetir a nova password</label>
-                                <input type="password" name="text_repeat_new_password" id="text_repeat_new_password" class="form-control" required>
+                                <input type="password" name="text_repeat_new_password" id="text_repeat_new_password"
+                                    class="form-control" required>
                             </div>
 
                         </div>
                     </div>
 
                     <div class="mb-3 text-center">
-                        <a href="#" class="btn btn-secondary px-3"><i class="fa-solid fa-xmark me-2"></i>Cancelar</a>
-                        <button type="submit" class="btn btn-secondary px-3"><i class="fa-solid fa-check me-2"></i>Alterar</button>
+                        <a href="?ct=main&mt=index" class="btn btn-secondary px-3"><i
+                                class="fa-solid fa-xmark me-2"></i>Cancelar</a>
+                        <button type="submit" class="btn btn-secondary px-3"><i
+                                class="fa-solid fa-check me-2"></i>Alterar</button>
                     </div>
 
                 </form>
 
+                <?php if (isset($validation_errors)) : ?>
                 <div class="alert alert-danger p-2 text-center">
-                    [mensagem de erro]
+                    <ul>
+                        <?php foreach ($validation_errors as $error) : ?>
+                        <li><?= $error ?></li>
+                        <?php endforeach; ?>
+                    </ul>
                 </div>
+                <?php endif; ?>
+
+                <?php if (isset($server_errors)) : ?>
+                <div class="alert alert-danger p-2 text-center">
+                    <ul>
+                        <?php foreach ($server_errors as $error) : ?>
+                        <li><?= $error ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+                <?php endif; ?>
 
             </div>
         </div>
